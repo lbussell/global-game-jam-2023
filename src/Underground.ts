9f4453e36b907f77e8549b5e9409c8c26a95d601
Logan Bussell <loganbussell@microsoft.com>
2023-01-31 21:37:35 -0800

    WIP

---
+++ src/Underground.ts
@@ -0,0 +1,83 @@
+import Phaser from "phaser";
+import * as Constants from './Constants';
+import {
+  Size,
+  Position,
+  TestTiles, 
+} from './Constants';
+
+export default class Underground {
+    private _tilemap: Phaser.Tilemaps.Tilemap;
+    private _scene: Phaser.Scene;
+    private _seed: number;
+
+    private _rootTileIndex: number;
+
+    constructor(scene: Phaser.Scene, seed: number = 0) {
+        this._scene = scene;
+        this._seed = seed;
+        this._rootTileIndex = 0;
+
+        this._tilemap = this._scene.make.tilemap({ 
+            tileWidth: Constants.TILE_SIZE,
+            tileHeight: Constants.TILE_SIZE,
+            width: 32,
+            height: 32
+        });
+
+        const tiles = this._tilemap.addTilesetImage(TestTiles.key);
+
+        const layerDirt = this._tilemap.createBlankLayer('dirt', tiles);
+        const layerRoot = this._tilemap.createBlankLayer('root', tiles);
+        layerDirt.setScale(Constants.TILE_SCALE);
+        layerRoot.setScale(Constants.TILE_SCALE);
+
+        layerDirt.fill(4, 0, 0, 32, 32);
+
+        layerRoot.putTileAt(0, 0, 0, true);
+        this._tilemap.putTileAt(this._rootTileIndex, 0, 1, true, 'root');
+
+        const testPos: Position = { x: 0, y: 1 }
+        this.placeRoot(testPos)
+    }
+
+    click(worldPoint: Phaser.Math.Vector2): boolean {
+        const pos: Position = {
+            x: this._tilemap.worldToTileX(worldPoint.x),
+            y: this._tilemap.worldToTileY(worldPoint.y)
+        };
+        return this.placeRoot(pos);
+    }
+
+    placeRoot(pos: Position): boolean {
+        // Verify adjacent root
+        let isAdjacent:boolean = false;
+        if (this._tilemap.getTileAt(pos.x + 1, pos.y, undefined, 'root') != null)
+        {
+            isAdjacent = true;
+        }
+
+        if (this._tilemap.getTileAt(pos.x - 1, pos.y, undefined, 'root') != null)
+        {
+            isAdjacent = true;
+        }
+
+        if (this._tilemap.getTileAt(pos.x, pos.y + 1, undefined, 'root') != null)
+        {
+            isAdjacent = true;
+        }
+
+        if (this._tilemap.getTileAt(pos.x, pos.y - 1, undefined, 'root') != null)
+        {
+            isAdjacent = true;
+        }
+
+        if (isAdjacent)
+        {
+            this._tilemap.putTileAt(this._rootTileIndex, pos.x, pos.y, true, 'root');
+        }
+        
+        // Return true if the action was successful
+        return isAdjacent;
+    }
+}
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,61 @@
 import Phaser from "phaser";
+import * as Constants from '../Constants';
+import {
+  Size,
+  Position,
+  TestTiles, 
+} from '../Constants';
+import Underground from '../Underground';
 
-export default class Demo extends Phaser.Scene {
+export default class World extends Phaser.Scene {
+
+  private isLoaded: boolean;
+  private underground?: Underground;
+  private clicked: boolean;
+
   constructor() {
     super("GameScene");
+    this.isLoaded = false;
   }
 
   preload() {
-    this.load.image("logo", "assets/phaser3-logo.png");
+    this.load.spritesheet(
+      TestTiles.key,
+      TestTiles.location,
+      { frameWidth: Constants.TILE_SIZE, frameHeight: Constants.TILE_SIZE }
+    );
+    this.isLoaded = true;
+  }
+
+  unload() {
+
   }
 
   create() {
-    const logo = this.add.image(400, 70, "logo");
+    this.underground = new Underground(this);
+  }
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: "Sine.inOut",
-      yoyo: true,
-      repeat: -1,
-    });
+  addSprite(key: string, pos: Position) {
+    this.add.sprite(pos.x, pos.y, key, 4).setOrigin(0.5).setScale(3);
+  }
+
+  update(time: number, delta: number): void {
+    if (this.isLoaded) {
+      const worldPoint: Phaser.Math.Vector2 = <Phaser.Math.Vector2> this.input.activePointer.positionToCamera(this.cameras.main);
+
+      if (this.input.manager.activePointer.isDown)
+      {
+        if (!this.clicked)
+        {
+          this.underground?.click(worldPoint);
+          this.clicked = true;
+        }
+      }
+      else
+      {
+        this.clicked = false;
+      }
+    }
   }
 }
+
